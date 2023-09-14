1a534c5312cc8ef2b3b373630e75fd78730e96c8
unknown <reskybobby04@gmail.com>
2023-09-14 15:39:19 +0800

    Tugas Praktikum 1

--- H071231060/Pertemuan 1/Nomor3.py
+++ H071231060/Pertemuan 1/Nomor3.py
@@ -3,7 +3,7 @@ a = float(input("masukkan nilai a="))
 b = float(input("masukkan nilai b="))
 c = float(input("masukkan nilai c="))
 
-x1 = -b + ((b*b-4*a*c)**0.5)/2*a
+x1 = (-b + ((b*b-4*a*c)**0.5))/2*a
 print("nilai dari x1 adalah", format (x1, ".2f"))
-x2 = -b - ((b*b-4*a*c)**0.5)/2*a
+x2 = (-b - ((b*b-4*a*c)**0.5))/2*a
 print("nilai dari x2 adalah", format (x2, ".2f"))
